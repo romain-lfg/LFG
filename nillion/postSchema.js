92cb65f4562c43cd6b2d2a639d54e37b8862eee7
Okulon <alex245@live.com>
2025-02-03 17:32:00 +0100

    Added Nillion
    Added basic storage and data retrieval with nillion,
    
    fixed issues that were preventing correct building

---
+++ nillion/postSchema.js
@@ -0,0 +1,25 @@
+import { SecretVaultWrapper } from 'nillion-sv-wrappers';
+import { orgConfig } from './nillionOrgConfig.js';
+import { readFile } from 'fs/promises';
+
+async function main() {
+  try {
+    const org = new SecretVaultWrapper(
+      orgConfig.nodes,
+      orgConfig.orgCredentials
+    );
+    await org.init();
+
+    // Create a new collection schema for all nodes in the org
+    const collectionName = 'Web3 Experience Survey';
+    const schema = JSON.parse(await readFile(new URL('./schema.json', import.meta.url)));
+    const newSchema = await org.createSchema(schema, collectionName);
+    console.log('✅ New Collection Schema created for all nodes:', newSchema);
+    console.log('👀 Schema ID:', newSchema[0].result.data);
+  } catch (error) {
+    console.error('❌ Failed to use SecretVaultWrapper:', error.message);
+    process.exit(1);
+  }
+} //scemaide 130e25d2-5d21-4f0d-ae56-dd6e634ea7a6
+
+main();
